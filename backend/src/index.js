fc2c2386679c763ff87aae04a048db931b4eabd4
Marcos Antônio de Souza Silva <chuckymass@gmail.com>
2020-01-16 16:40:30 -0300

    Add estrutura básica web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 const dbUrl = 'mongodb+srv://massilva:gratis@cluster0-bo5ev.mongodb.net/test?retryWrites=true&w=majority';
@@ -10,6 +11,7 @@ mongoose.connect(dbUrl, {
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
